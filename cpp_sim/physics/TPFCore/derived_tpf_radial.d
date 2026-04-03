f1eb8b9532a7d6ce50dab582436d0508e4ae438d
Daniel Rochatka <daniel@rochatka.com>
2026-04-03 03:11:31 -0400

    Stop tracking generated build artifacts

--- cpp_sim/physics/TPFCore/derived_tpf_radial.d
+++
@@ -1,8 +0,0 @@
-physics/TPFCore/derived_tpf_radial.o: \
- physics/TPFCore/derived_tpf_radial.cpp \
- physics/TPFCore/derived_tpf_radial.hpp physics/TPFCore/source_ansatz.hpp \
- physics/TPFCore/../../types.hpp physics/TPFCore/../../config.hpp
-physics/TPFCore/derived_tpf_radial.hpp:
-physics/TPFCore/source_ansatz.hpp:
-physics/TPFCore/../../types.hpp:
-physics/TPFCore/../../config.hpp:
